0be1b426b371dfc9eb401864dd26d7527392b66b
Colin Matteson <seefm@proton.me>
2024-08-12 23:01:41 -0400

    adding more question and answer buildout

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,23 @@ import { getAllPlayers } from './services/playerService'
 function App() {
   const [players, setPlayers] = useState([])
   const [selectedPlayers, setSelectedPlayers] = useState([])
+  const [answer, setAnswer] = useState({})
 
   const pickRandomPlayers = (playerArray) => {
     let somePlayers = [];
     let someIndexes = []
     for (let i = 0; i < 4; i++) {
-      let ranNum = Math.floor((Math.random() * playerArray.length) + 1);
+      let ranNum = Math.floor((Math.random() * playerArray.length));
       if (someIndexes.includes(ranNum)) {
-        ranNum = Math.floor((Math.random() * playerArray.length) + 1);
+        ranNum = Math.floor((Math.random() * playerArray.length));
         somePlayers.push(playerArray[ranNum]);
       } else {
         somePlayers.push(playerArray[ranNum]);
       }
     }
     setSelectedPlayers(somePlayers);
-
+    let anotherRanNum = Math.floor(Math.random() * 4)
+    setAnswer(somePlayers[anotherRanNum]);
   }
 
 
@@ -35,9 +37,11 @@ function App() {
 
   return (
     <>
-    <p>hello</p>
+    <div>RIDDLE ME THIS, BALLER</div>
+
+    <div>{answer?.name}</div>
     {selectedPlayers.map((nba) => 
-      <div>{nba?.name}</div>
+      <button id={'player-' + nba?.id}>{nba?.name}</button>
     )}
     </>
   )
